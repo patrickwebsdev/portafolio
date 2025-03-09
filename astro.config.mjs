d502548f65c47be6e02cf0201b348701f0bec6bf
patrickwebsdev <contacto@patricioalmada.com.ar>
2025-03-09 16:32:51 -0300

    refactor: change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://patrickwebsdev.github.io',
-  base: '/',
+  site: "https://patrickwebsdev.github.io",
+  base: "/portafolio",
 });
