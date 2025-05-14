cbb0148f8302b6afc63ecc36c6b567e83b03e6a2
patrickwebsdev <contacto@patricioalmada.com.ar>
2025-05-13 21:12:05 -0300

    fix: domain site & base in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://patrickwebsdev.github.io",
-  base: "/portafolio",
+  site: "https://patricioalmada.com.ar",
+  base: "/",
 });
